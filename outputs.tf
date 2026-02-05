3702ea9b24e34eca86eaba3fe81e3fe486fe3f62
root <root@EC2AMAZ-U70TDO6.localdomain>
2026-02-05 23:00:36 +0000

    Added Files

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "Applicant name"
+  value       = var.name
+}
+
+output "position" {
+  description = "Position being applied for"
+  value       = var.position
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  description = "Applicant name"
+  type        = string
+  default     = "Shiva Malladi"
+}
+
+variable "position" {
+  description = "Position being applied for"
+  type        = string
+  default     = "Sr DevOps Engineer"
+}
